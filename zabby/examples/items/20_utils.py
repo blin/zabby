701a6fdccb5dc008dff33c0500585a229094b730
Evgeniy Petrov <blin@f4oe.org>
2013-05-19 19:09:49 +0400

    Added examples for tcp_communcation and exception_guard

--- zabby/examples/items/20_utils.py
+++ zabby/examples/items/20_utils.py
@@ -1,5 +1,17 @@
-from zabby.core.utils import sh
+# from zabby.core.six import b
+# from zabby.core.exceptions import OperatingSystemError
+# from zabby.core.utils import sh, exception_guard, tcp_communication
+
+
+# def redis_ping():
+#     responses = tcp_communication(6379, requests=[b('ping\r\n')])
+#     got_pong = b('+PONG\r\n') in responses
+#     return int(got_pong)
+
 
 items = {
-    #'cat': sh('cat {0}')
+    # 'cat': sh('cat {0}'),
+    # 'sleep': exception_guard(sh('sleep 1; echo 1', timeout=0.5),
+    #                          OperatingSystemError, 0),
+    # 'redis.ping': exception_guard(redis_ping, IOError, 0)
 }

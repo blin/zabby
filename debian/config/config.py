6ad54b18ad79a4fd6ef4e64948ee9609c8c34703
Maxim Filatov <pipopolam@gmail.com>
2013-09-09 12:18:01 +0400

    Debian folder

---
+++ debian/config/config.py
@@ -0,0 +1,13 @@
+import os
+
+listen_host = '0.0.0.0'
+listen_port = 10052
+
+_config_dir = os.path.dirname(os.path.abspath(__file__))
+
+_item_dir = os.path.join(_config_dir, 'items')
+item_files = sorted([os.path.join(_item_dir, item_file)
+                     for item_file in os.listdir(_item_dir)
+                     if item_file.endswith('.py')])
+
+logging_conf = os.path.join(_config_dir, 'logging.conf')

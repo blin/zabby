903b56cdd918b21cd34c2cdd5ba5c12edcc615b3
Evgeniy Petrov <blin@f4oe.org>
2013-10-29 22:06:30 +0400

    Added stream redirection and file closing to daemonization
    As PEP 3143 is deferred, further daemonization steps are copied
    from reference implementation

--- zabby/cli.py
+++ zabby/cli.py
@@ -1,6 +1,8 @@
 import atexit
 import optparse
 import os
+import subprocess
+import sys
 
 
 option_parser = optparse.OptionParser()
@@ -20,13 +22,26 @@ option_parser.add_option(
     help='Specifies where to store pid',
     default='/var/run/zabby/zabby.pid',
 )
+option_parser.add_option(
+    '--error-log',
+    help='Specifies where stdout and stderr will be redirected '
+         'if zabby is running as independent process',
+    default='/var/log/zabby/zabby.err',
+)
 
 
-def daemonize(pid_file):
+def daemonize(pid_file, error_log):
     def fork_exit_parent():
         pid = os.fork()
         if pid > 0:
             os._exit(0)
+
+    def redirect_stream(system_stream, target_stream):
+        os.dup2(target_stream.fileno(), system_stream.fileno())
+
+    def close_all_open_files():
+        '''Close all file descriptors except for stdin, stdout, stderr'''
+        os.closerange(3, subprocess.MAXFD)
 
     if os.path.exists(pid_file):
         raise RuntimeError("Pid file {0} already exists".format(pid_file))
@@ -34,6 +49,15 @@ def daemonize(pid_file):
     fork_exit_parent()
     os.setsid()
     fork_exit_parent()
+
+    close_all_open_files()
+
+    with open(os.devnull) as devnull:
+        redirect_stream(sys.stdin, devnull)
+
+    with open(error_log, 'a') as error_log_file:
+        redirect_stream(sys.stdout, error_log_file)
+        redirect_stream(sys.stderr, error_log_file)
 
     with open(pid_file, 'w') as f:
         f.write(str(os.getpid()))
